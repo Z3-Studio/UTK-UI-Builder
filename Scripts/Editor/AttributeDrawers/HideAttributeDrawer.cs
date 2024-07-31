65f720c2961bd7ca4287b13fa5626bc52825dd2b
André Felipe Siqueira <andrefs.995@gmail.com>
2024-08-01 00:02:49 +0100

    Hide Attribute implementation

---
+++ Scripts/Editor/AttributeDrawers/HideAttributeDrawer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.UIElements;
+using Z3.UIBuilder.Core;
+
+namespace Z3.UIBuilder.Editor
+{
+    public class HideAttributeDrawer : Z3AttributeDrawer<HideAttribute>
+    {
+        protected override void Draw()
+        {
+            VisualElement.style.display = DisplayStyle.None;
+        }
+    }
+}
--- Scripts/Runtime/Attributes/Z3VisualElementAttribute.cs
+++ Scripts/Runtime/Attributes/Z3VisualElementAttribute.cs
@@ -3,5 +3,10 @@
     /// <summary>
     /// Created to make adding multiple <see cref="UnityEngine.PropertyAttribute"/> easy
     /// </summary>
+    /// <remarks>
+    /// NOTE: It doesn't work with collection. In this case you have to use PropertyCollectionAttribute
+    /// <para> Luckily in version 2023.3 or Unity 6 there will be a new attribute that works with both! </para>
+    /// </remarks>
+    /// https://discussions.unity.com/t/please-replace-propertycollectionattribute-with-a-bool-property-in-propertyattribute/919513
     public class Z3VisualElementAttribute : UnityEngine.PropertyAttribute { }
 }
