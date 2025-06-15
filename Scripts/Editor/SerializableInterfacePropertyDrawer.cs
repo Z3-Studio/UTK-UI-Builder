45875a3757599f3aaf4543af37d8881dbc06f163
Master Z <andrefs.995@gmail.com>
2025-06-15 11:47:30 -0300

    Serialized Interface

--- Scripts/Editor/SerializableInterfacePropertyDrawer.cs
+++ Scripts/Editor/SerializableInterfacePropertyDrawer.cs
@@ -1,7 +1,10 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEditor.UIElements;
+using UnityEngine;
 using UnityEngine.UIElements;
 using Z3.UIBuilder.Core;
+using Object = UnityEngine.Object;
 
 namespace Z3.UIBuilder.Editor
 {
@@ -11,17 +14,74 @@ namespace Z3.UIBuilder.Editor
     [CustomPropertyDrawer(typeof(SerializableInterface<>), true)]
     public class SerializableInterfacePropertyDrawer : Z3PropertyDrawer<ISerializableInterface>
     {
+        private Type InterfaceType => ResolvedValue.InterfaceType;
+
+        private ObjectField objectField;
+        private VisualElement display;
+
+        private const string DisplayClass = "unity-object-field-display";
+        private const string AcceptDropClass = "unity-object-field-display--accept-drop";
+        private const string FieldPath = "serializedObject";
+
         protected override VisualElement CreateVisualElement()
         {
-            ObjectField objectField = new()
+            objectField = new()
             {
                 value = ResolvedValue.SerializedObject,
-                objectType = ResolvedValue.InterfaceType,
+                objectType = InterfaceType,
                 label = SerializedProperty.displayName
             };
-            objectField.BindProperty(SerializedProperty);
+
+            // Setup bind
+            SerializedProperty serializedObjectField = SerializedProperty.FindPropertyRelative(FieldPath);
+            objectField.BindProperty(serializedObjectField);
+
+            // Prepare ObjectFieldDisplay
+            display = objectField.Q(null, DisplayClass);
+
+            display.RegisterCallback<DragUpdatedEvent>(ValidateDrag, TrickleDown.TrickleDown);
+            display.RegisterCallback<DragPerformEvent>(ValidateDrag, TrickleDown.TrickleDown);
+            display.RegisterCallback<DragLeaveEvent>(OnDragLeave, TrickleDown.TrickleDown);
 
             return objectField;
+        }
+
+        private void ValidateDrag(EventBase e)
+        {
+            if (DragAndDrop.objectReferences.Length == 0) 
+                return;
+
+            Object obj = GetObjectWithInterface(DragAndDrop.objectReferences[0]);
+            bool ok = obj != null;
+            DragAndDrop.visualMode = ok ? DragAndDropVisualMode.Copy : DragAndDropVisualMode.Rejected;
+
+            if (ok && e is DragPerformEvent)
+            {
+                objectField.value = obj;
+                display.EnableInClassList(AcceptDropClass, false);
+            }
+            else
+            {
+                display.EnableInClassList(AcceptDropClass, ok);
+            }
+        }
+
+        /// <summary> Used to remove Accetable drop color </summary>
+        private void OnDragLeave(DragLeaveEvent e) => display.EnableInClassList(AcceptDropClass, false);
+
+        /// <summary> Find Object to be serialized, if exist </summary>
+        private Object GetObjectWithInterface(Object obj)
+        {
+            if (obj == null)
+                return null;
+
+            if (InterfaceType.IsAssignableFrom(obj.GetType()))
+                return obj;
+
+            if (obj is GameObject go && go.TryGetComponent(InterfaceType, out Component comp))
+                return comp;
+
+            return null;
         }
     }
 }
