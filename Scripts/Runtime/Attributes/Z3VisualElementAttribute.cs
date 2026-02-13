2d30d31cb62a45bd5303abd40c9b48e60fd030a3
Master Z <andrefs.995@gmail.com>
2026-02-12 23:28:47 -0300

    MAJOR CHANGE

--- Scripts/Runtime/Attributes/Z3VisualElementAttribute.cs
+++ Scripts/Runtime/Attributes/Z3VisualElementAttribute.cs
@@ -1,4 +1,6 @@
-﻿namespace Z3.UIBuilder.Core
+﻿using System;
+
+namespace Z3.UIBuilder.Core
 {
     /// <summary>
     /// Created to make adding multiple <see cref="UnityEngine.PropertyAttribute"/> easy
@@ -8,5 +10,5 @@
     /// <para> Luckily in version 2023.3 or Unity 6 there will be a new attribute that works with both! </para>
     /// </remarks>
     /// https://discussions.unity.com/t/please-replace-propertycollectionattribute-with-a-bool-property-in-propertyattribute/919513
-    public class Z3VisualElementAttribute : UnityEngine.PropertyAttribute { }
+    public class Z3VisualElementAttribute : Attribute { }
 }
